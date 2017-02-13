c459f467114e9ce78cc2d22b786fbb3bff360bf0
TJ Carney <tjcarney@TJs-MacBook-Air.local>
2017-02-13 09:14:20 -0500

    Completed Lab and Ran App Successfully

--- Cats/Cats/ViewController.swift
+++ Cats/Cats/ViewController.swift
@@ -10,7 +10,27 @@ import UIKit
 
 class ViewController: UITableViewController {
     
+    let cats = ["Abyssinian", "American Shorthair", "Bengal", "Birman", "British Shorthair", "Burmese", "Exotic Shorthair", "Maine Coon", "Norwegian Forest", "Persian", "Ragdoll", "Scottish Fold"]
     
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cats.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "KittyCell", for: indexPath)
+        
+        let currentCat = cats[indexPath.row]
+        
+        cell.textLabel?.text = currentCat
+        
+        return cell
+        
+    }
    
     
     
